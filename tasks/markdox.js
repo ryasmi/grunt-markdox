9cc55f380667d41e229603754b71473af71d55c0
Ryan Smith <0ryansmith1994@gmail.com>
2014-01-19 23:06:32 +0000

    Using built in grunt functionality for globbing.
    Adds functionality for multiple output destinations, Closes #2.
    
    See
    http://gruntjs.com/configuring-tasks#building-the-files-object-dynamical
    ly

--- tasks/markdox.js
+++ tasks/markdox.js
@@ -3,18 +3,45 @@ module.exports = function (grunt) {
 
     grunt.registerMultiTask('markdox', 'A grunt task for running markdox.', function () {
         var markdox = require('markdox');
-        var glob = require('glob');
         var options = this.options({});
+        var validFiles = removeInvalidFiles(this.files);
         var done = this.async();
+        var complete = (function () {
+            var remaining = validFiles.length;
 
-        options.output = this.data.dest || options.output;
-        grunt.file.write(options.output, '');
+            return function () {
+                remaining -= 1;
+                if (!remaining) {
+                    grunt.log.writeln('Documentation generated.');
+                    done();
+                }
+            };
+        }());
 
-        glob(this.data.src, function (err, files) {
-            markdox.process(files, options, function () {
-                grunt.log.writeln('Documentation generated.');
-                done();
+        validFiles.forEach(function (f) {
+            options.output = f.dest;
+
+            if (!grunt.file.exists(f.dest)) {
+                grunt.file.write(f.dest, '');
+            }
+
+            grunt.log.writeln(f.src + ' : ' + f.dest);
+
+            markdox.process(f.src, options, function () {
+                complete();
             });
         });
     });
+
+    var removeInvalidFiles = function (files) {
+        return files.filter(function (f) {
+            if (!f.src.length || !grunt.file.exists(f.src)) {
+                grunt.log.warn('"' + f.src + '" not found.');
+                return false;
+            } else {
+                grunt.log.writeln(f.src + ' exists.');
+                return true;
+            }
+        });
+    };
 };
